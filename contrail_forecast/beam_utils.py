7cf329b5bafa1cf2ad7278f1931d2489baa81d80
Tharun Sankar <tsankar@google.com>
2023-09-14 21:21:05 +0000

    allow for potential met data caching

--- contrail_forecast/beam_utils.py
+++ contrail_forecast/beam_utils.py
@@ -2,7 +2,7 @@
 
 import apache_beam as beam
 import numpy as np
-from typing import Iterable
+from typing import Any, Iterable
 import utils
 import xarray as xr
 from pycontrails.models.cocipgrid import CocipGrid
@@ -17,27 +17,33 @@ def _fix_attrs(ds: xr.Dataset) -> xr.Dataset:
     return ds
 
 
-def eval_task(t: np.datetime64, seed: int) -> Iterable[tuple[str, xr.Dataset]]:
-    """Evaluate the task.
+class CocipGridFn(beam.DoFn):
+    """Runs CocipGrid on a single set of inputs."""
 
-    Should be run in some data processing pipeline.
-    """
-    met = utils.open_met()
-    rad = utils.open_rad()
+    def __init__(self, configs: dict[str, Any]):
+        # If weather is opened with a cache, it will persist until the worker
+        # dies. Make sure weather is loaded lazily, though! This is run by the
+        # manager, and the serialized DoFn is sent to the workers as a "starting
+        # point."
+        self.met = utils.open_met()
+        self.rad = utils.open_rad()
+    
+    def process(self, indata: tuple[np.datetime64, int]) -> Iterable[tuple[str, xr.Dataset]]:
+        """Evaluates the task. `indata` should be a tuple of (timestamp, seed)."""
+        t, seed = indata
+        rng = np.random.default_rng(seed)
+        params = utils.create_cocip_grid_params(rng)
 
-    rng = np.random.default_rng(seed)
-    params = utils.create_cocip_grid_params(rng)
+        model = CocipGrid(self.met, self.rad, params)
 
-    model = CocipGrid(met, rad, params)
+        source = utils.create_source(t)
+        mds = model.eval(source)
 
-    source = utils.create_source(t)
-    mds = model.eval(source)
+        ds = mds.data
+        ds.attrs["seed"] = seed
+        ds = _fix_attrs(ds)
 
-    ds = mds.data
-    ds.attrs["seed"] = seed
-    ds = _fix_attrs(ds)
-
-    yield str(t), ds
+        yield str(t), ds
 
 
 def run_cocipgrid(root: beam.Pipeline) -> beam.Pipeline:
@@ -46,7 +52,8 @@ def run_cocipgrid(root: beam.Pipeline) -> beam.Pipeline:
     t = np.datetime64("2023-05-07T09:00:00")
 
     rng = np.random.default_rng(None)
-    n_rollouts = utils.load_configs()["monte_carlo"]["n_rollouts"]
+    configs = utils.load_configs()
+    n_rollouts = configs["monte_carlo"]["n_rollouts"]
     seeds = rng.integers(0, 2**32, size=n_rollouts)
 
     rollout_inputs = [(t, seed) for seed in seeds]
@@ -57,5 +64,5 @@ def run_cocipgrid(root: beam.Pipeline) -> beam.Pipeline:
     return (
         root
         | "CreateSeeds" >> beam.Create(rollout_inputs)
-        | "Evaluate" >> beam.FlatMapTuple(eval_task)
+        | "Evaluate" >> beam.ParDo(CocipGridFn(configs))
     )
